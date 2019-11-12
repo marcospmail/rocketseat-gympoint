6c40cd26cdc6af9c70a80fa84a1198dc3d040e1d
Marcos Paulo <marcosp.mail@gmail.com>
2019-11-12 15:00:06 -0300

    fix: Bug in registration relation with plan

--- backend/src/app/models/Registration.js
+++ backend/src/app/models/Registration.js
@@ -27,7 +27,7 @@ class Registration extends Model {
 
   static associate(models) {
     this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
-    this.belongsTo(models.Student, { foreignKey: 'plan_id', as: 'plan' });
+    this.belongsTo(models.Plan, { foreignKey: 'plan_id', as: 'plan' });
   }
 }
 
