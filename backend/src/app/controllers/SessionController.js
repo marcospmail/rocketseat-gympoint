b5f8f9d9d9d2795fdf8c8d78acc6e684228991c0
Marcos Paulo <marcosp.mail@gmail.com>
2019-12-09 20:37:50 -0300

    chore: Add try/catch to SessionController.store()

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -7,43 +7,47 @@ import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
-    const validateSchema = requestBody => {
-      const schema = Yup.object().shape({
-        email: Yup.string().required(),
-        password: Yup.string().required(),
+    try {
+      const validateSchema = requestBody => {
+        const schema = Yup.object().shape({
+          email: Yup.string().required(),
+          password: Yup.string().required(),
+        });
+
+        return schema.isValid(requestBody);
+      };
+
+      if (!(await validateSchema(req.body))) {
+        return res.status(400).json({ error: 'Validação falhou' });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        return res.status(400).json({ error: 'Usuário não encontrado' });
+      }
+
+      if (!(await user.checkPassword(password))) {
+        return res.status(400).json({ error: 'Password inválido' });
+      }
+
+      const { id, name } = user;
+
+      return res.json({
+        user: {
+          id,
+          name,
+          email,
+        },
+        token: jwt.sign({ id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
       });
-
-      return schema.isValid(requestBody);
-    };
-
-    if (!(await validateSchema(req.body))) {
-      return res.status(400).json({ error: 'Validação falhou' });
+    } catch (err) {
+      console.log(err);
     }
-
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ where: { email } });
-
-    if (!user) {
-      return res.status(400).json({ error: 'Usuário não encontrado' });
-    }
-
-    if (!(await user.checkPassword(password))) {
-      return res.status(400).json({ error: 'Password inválido' });
-    }
-
-    const { id, name } = user;
-
-    return res.json({
-      user: {
-        id,
-        name,
-        email,
-      },
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
-    });
   }
 }
 
