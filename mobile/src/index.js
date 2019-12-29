6af48fbbc69be24cfca28ae77fb9003a8efcda5a
Marcos Paulo <marcosp.mail@gmail.com>
2019-12-29 13:40:04 -0300

    chore: Removed CodePush stuff

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'react-native';
-
-import CodePush from 'react-native-code-push';
-import OneSignal from 'react-native-onesignal';
 
 import '~/config/ReactotronConfig';
 
@@ -12,41 +9,13 @@ import App from '~/App';
 
 import { store, persistor } from '~/store';
 
-class Index extends Component {
-  constructor(props) {
-    super(props);
-
-    OneSignal.init('2f813a91-69f5-4edf-9983-9e8b63e9165e');
-
-    OneSignal.addEventListener('received', this.onReceived);
-    OneSignal.addEventListener('opened', this.onOpened);
-    OneSignal.addEventListener('ids', this.onIds);
-  }
-
-  componentWillUnmount() {
-    OneSignal.removeEventListener('received', this.onReceived);
-    OneSignal.removeEventListener('opened', this.onOpened);
-    OneSignal.removeEventListener('ids', this.onIds);
-  }
-
-  onReceived = data => {};
-
-  onOpened = notification => {};
-
-  onIds = id => {};
-
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
-          <App />
-        </PersistGate>
-      </Provider>
-    );
-  }
+export default function Index() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+        <App />
+      </PersistGate>
+    </Provider>
+  );
 }
-
-export default CodePush({
-  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
-})(Index);
